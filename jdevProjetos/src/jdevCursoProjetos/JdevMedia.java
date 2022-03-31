ee55851bb27448c1606b7a07de40ee1fc0b38524
matheus <matheus@10.0.0.100>
2022-03-31 08:00:36 -0300

    Projeto de calculo de media adicionado as variaveis e os dialogos.

--- jdevProjetos/src/jdevCursoProjetos/JdevMedia.java
+++ jdevProjetos/src/jdevCursoProjetos/JdevMedia.java
@@ -1,9 +1,40 @@
 package jdevCursoProjetos;
 
+import javax.swing.JOptionPane;
+
 public class JdevMedia {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+
+		/* estanciado as variaveis de nota */
+
+		String nota1 = JOptionPane.showInputDialog("Digite a nota n1");
+		String nota2 = JOptionPane.showInputDialog("Digite a nota n2");
+		String nota3 = JOptionPane.showInputDialog("Digite a nota n3");
+		String nota4 = JOptionPane.showInputDialog("Digite a nota n4");
+
+		/* convertendo as variaveis para double */
+
+		double dNota1 = Double.parseDouble(nota1);
+		double dNota2 = Double.parseDouble(nota2);
+		double dNota3 = Double.parseDouble(nota3);
+		double dNota4 = Double.parseDouble(nota4);
+
+		double media = (dNota1 + dNota2 + dNota3 + dNota4) / 4;
+
+		if (media > 5) {
+
+			if (media >= 7) {
+				JOptionPane.showMessageDialog(null, "A aluno aprovado com media: " + media);
+			}
+			if (media >=5 && media < 7) {
+				JOptionPane.showMessageDialog(null, "A aluno em recuperação com media: " + media);
+			}
+
+		} else {
+			JOptionPane.showMessageDialog(null, "A aluno reprovado com media: " + media);
+		}
 
 	}
 
